ef25a18eee766465cf2545638226a78a54edb595
Merijn Testroote <hi@merijn.site>
2024-05-14 08:21:05 +0200

    net

--- net_deactivator.py
+++ net_deactivator.py
@@ -23,7 +23,8 @@ def plot_model(x, y, model, plot=None):
 
     ax.set_xlabel('Predicted')
     ax.set_ylabel('Actual')
-    
+   # ax.set_xlim((-10, 755000.0))
+    ax.set_ylim((-10, 755000.0)) 
     #ax.scatter(x1, y, label='data')
     ax.scatter(model.forward(x), y, label='prediction', marker='+')
     
@@ -62,7 +63,7 @@ def relu(x):
     return np.maximum(0, x)
 
 def relu_d(x):
-    return np.where(x > 0, 1, 0)
+    return np.where(x > 0, 1, 0.001)
 
 def sigmoid(x):
     return np.log(1 + np.exp(x))
@@ -73,10 +74,10 @@ def sigmoid_d(x):
 
 
 class Model:
-    def __init__(self, hidden_size):
-        self.w = np.random.normal(size=hidden_size * 2).reshape(2,-1)
+    def __init__(self, input_size, hidden_size):
+        self.w = np.random.normal(4,size=hidden_size * input_size).reshape(input_size,-1)
         self.b = np.zeros((hidden_size))
-        self.w_output = np.random.normal(size=hidden_size).reshape(-1,1)
+        self.w_output = np.random.normal(4, size=hidden_size).reshape(-1,1)
         self.linear_cache = None
         self.activation = sigmoid
         self.activation_d = sigmoid_d
@@ -90,8 +91,8 @@ class Model:
         self.linear_cache = None
     
     def linear_pass(self, x):
-        if self.linear_cache is None:
-            self.linear_cache = x @ self.w + self.b
+        #if self.linear_cache is None:
+        self.linear_cache = x @ self.w + self.b
         return self.linear_cache
     
     def forward(self, x):
@@ -102,17 +103,17 @@ class Model:
         return self.activation(self.linear_pass(x))
 
     def derrivative_w(self, x, i, j):
-        return self.activation_d(self.linear_pass(x))[i, [j]] * (x)
+        return self.activation_d(self.linear_pass(x))[0,j] * x[0,i]
 
     def derrivative_b(self, x, i):
-        return self.activation_d(self.linear_pass(x))
+        return self.activation_d(self.linear_pass(x))[0,i]
     
   
 
 def mae_d(x, y, model):
     y_predict = model.forward(x)
     return (np.array([np.sum(abs_d((y_predict) - y) 
-                                   * model.derrivative_w(x,i, j))
+                                   * model.derrivative_w(x, i, j))
                       for i in range(model.w.shape[0])
                       for j in range(model.w.shape[1])]
                         ).reshape(model.w.shape), # dw
@@ -123,9 +124,10 @@ def mae_d(x, y, model):
                       * model.derrivative_w_output(x), axis=0)) # db
 
 #%% 
-df = pd.read_csv('trainingsset.csv')
-xy = (df[['actuele_diepgang', 'lengte_schip_huidig', 'reistijd_totaal']]
-        .replace('#', '-1.0')
+df = (pd.read_csv('train.csv').select_dtypes(include='number'))
+xy = (df
+        #.replace('#', '-1.0')
+        .fillna(0)
         .astype(np.float32)
         .to_numpy()
         )
@@ -133,24 +135,35 @@ np.random.shuffle(xy)
 split_idx = round(xy.shape[0] * 0.7)
 train = xy[:split_idx,:]
 test = xy[split_idx:]
-x = train[:, :-1]
-y = train[:, [-1]]
 
-# %%
-hidden_size = 10
-model = Model(hidden_size)
+
+hidden_size = 30
+model = Model(xy.shape[1]-1, hidden_size)
 #plot = plot_model(x, y, model)
 
-u = 0.0001
+u = 0.0000001
 
 maes = []
-iterations = 500
-for i in range(iterations):
-    grad_w, grad_b, grad_w_ouput = mae_d(x, y, model)
-    model.grad_update(u * grad_w,
-                      u * grad_b,
-                      u * grad_w_ouput)
-    maes.append(mae(x, y, model))
+iterations = 1000
+for iter in range(iterations):
+    xys = train.copy()
+    np.random.shuffle(xys)
+    for i in range(xys.shape[0]):
+        x = xys[[i], :-1]
+        y = xys[[i], [-1]]
+        
+        grad_w, grad_b, grad_w_ouput = mae_d(x, y, model)
+        mae_s = mae(x, y, model)
+        mae_term = u * mae_s * 0
+        model.grad_update(u  * grad_w,
+                          u  * grad_b,
+                          u  * grad_w_ouput)
+    
+    x = xys[:, :-1]
+    y = xys[:, [-1]]
+    mae_it = mae(x, y, model)
+    print(f'Iteration {iter} finished with MAE: {mae_it}')
+    maes.append(mae_it)
 
 
 print(f'W: {model.w} \nb: {model.b}, \nMAE: {min(maes)}')
@@ -158,8 +171,8 @@ print(f'W-output: {model.w_output}')
 plot_model(x, y, model)
 
 
-# %%
-plt.plot(np.arange(iterations), maes)
+2# %%
+plt.scatter(np.arange(len(maes)), maes)
 print(min(maes))
 
 
